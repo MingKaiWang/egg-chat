4a7e5000f9dba302f9db9b7b00d82eca2a114c06
D8Ge <burnedfrog@163.com>
2018-03-29 17:59:40 +0800

    [CHANGE] 修改首页布局

--- web/egg-chat/src/router/index.js
+++ web/egg-chat/src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import iView from 'iview'
 
 Vue.use(Router)
+Vue.use(iView)
 
 export default new Router({
   routes: [
