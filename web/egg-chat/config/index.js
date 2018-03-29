5f56a0fc8a90c4da4ccc8899a83c1d73472353d5
D8Ge <burnedfrog@163.com>
2018-03-29 14:53:06 +0800

    [FIXED] 修复webpack打包的配置引起的访问不到资源的问题

--- web/egg-chat/config/index.js
+++ web/egg-chat/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../../../server/app/public'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/public',
 
     /**
      * Source Maps
