5e67fbff3bd22c5c20e259ed3280240d8fc29843
叶永平 <burnedfrog@163.com>
2018-04-15 21:53:51 +0800

    [NEW] 添加room 模型

---
+++ server/app/model/room.js
@@ -0,0 +1,12 @@
+module.exports = app => {
+    const mongoose = app.mongoose
+    const Schema = mongoose.Schema
+    
+    const RoomSchema = new Schema({
+        name: { type: String },
+        owner: { type: String }
+    }, {
+        timestamps: true
+    });
+    return mongoose.model('Message', RoomSchema)
+}
