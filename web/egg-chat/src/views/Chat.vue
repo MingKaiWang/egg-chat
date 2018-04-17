3b8f5b05b812cc2f7f33fccc709f40487d3807c3
D8Ge <burnedfrog@163.com>
2018-04-17 18:02:08 +0800

    [FIXED] add chat page

--- web/egg-chat/src/views/Chat.vue
+++ web/egg-chat/src/views/Chat.vue
@@ -1,10 +1,15 @@
 <template>
-  <row type='flex'>
+  <row type='flex' class='content'>
     <i-col span='6'>
-      side bar
+      <Tabs type='card'>
+        <TabPane class='list' label="聊天" name="name1">聊天配置</TabPane>
+        <TabPane class='list' label="好友" name="name2">好友列表</TabPane>
+        <TabPane class='list' label="设置" name="name3">设置信息</TabPane>
+      </Tabs>
     </i-col>
     <i-col span='18'>
-      chat content
+      <p>聊天标题</p>
+      <hr class='titleline'>
     </i-col>
   </row>
 </template>
@@ -19,5 +24,15 @@ export default {
 </script>
 
 <style scoped>
-
+.titleline {
+  margin-top: 13px;
+  border: 0.3px solid #dddee1
+}
+.list {
+  height: 100%;
+}
+.content {
+  width: 100%;
+  height: 100%;
+}
 </style>
